3c7667db239e245ff9ae06cdd9003650ed999587
Serhii Kostiuk <serhiikostiuk@Serhii-Kostiuk-iMac.local>
2016-11-07 18:35:23 +0200

    add root coordinator

---
+++ Source/Coordinators/RootCoordinator/RootCoordinator.h
@@ -0,0 +1,13 @@
+//
+//  RootCoordinator.h
+//  testCollage
+//
+//  Created by Serhii Kostiuk on 07.11.16.
+//  Copyright © 2016 Serhii Kostiuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RootCoordinator : NSObject
+
+@end
---
+++ Source/Coordinators/RootCoordinator/RootCoordinator.m
@@ -0,0 +1,13 @@
+//
+//  RootCoordinator.m
+//  testCollage
+//
+//  Created by Serhii Kostiuk on 07.11.16.
+//  Copyright © 2016 Serhii Kostiuk. All rights reserved.
+//
+
+#import "RootCoordinator.h"
+
+@implementation RootCoordinator
+
+@end
